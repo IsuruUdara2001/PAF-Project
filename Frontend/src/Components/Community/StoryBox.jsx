eb9b98389b80f2d8eb93d784dedd94ce21a725ae
ChamikaMelan <melanwijesinghe4@gmail.com>
2025-05-04 21:38:39 +0530

    update
    update

---
+++ Frontend/src/Components/Community/StoryBox.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useSnapshot } from "valtio";
+import state from "../../Utils/Store";
+import StoryCard from "./StoryCard";
+
+const StoryBox = () => {
+  const snap = useSnapshot(state);
+  return (
+    <div>
+      <p style={{ fontWeight: "bold", fontSize: "30px" }}>Learning Plan Sharing</p>
+
+      <div class="top_box">
+        <div
+          onClick={() => {
+            state.createWorkoutStatusModalOpened = true;
+          }}
+          class="my_story_card"
+        >
+          <img alt="alt-tag" src={snap.currentUser?.image} />
+
+          <div class="story_upload">
+           <br></br>
+          
+            <p class="story_tag" style={{ textAlign: "center", fontSize: 10 }}>
+              Create Learning story
+            </p>
+          </div>
+        </div>
+        {snap.storyCards.map((card) => (
+          <StoryCard key={card?.id} card={card} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default StoryBox;
