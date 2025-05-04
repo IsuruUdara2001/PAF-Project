9688ebea5d3aba545c4b5a61e977eca4506a79a7
ChamikaMelan <melanwijesinghe4@gmail.com>
2025-05-04 21:20:18 +0530

    update
    update

--- Frontend/src/Services/StoryService.js
+++ Frontend/src/Services/StoryService.js
@@ -39,58 +39,7 @@ class StoryUpdateService {
     }
   }
 
-  async getAllWorkoutStories() {
-    try {
-      const accessToken = localStorage.getItem("accessToken");
-      const config = {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      };
-      const response = await axios.get(
-        `${BASE_URL}/workoutStatusUpdates`,
-        config
-      );
-      return response.data;
-    } catch (error) {
-      throw new Error("Failed to get all Learning Plan stories");
-    }
-  }
-
-  async deleteWorkoutStory(updateId) {
-    try {
-      const accessToken = localStorage.getItem("accessToken");
-      const config = {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      };
-      await axios.delete(
-        `${BASE_URL}/workoutStatusUpdates/${updateId}`,
-        config
-      );
-    } catch (error) {
-      throw new Error("Failed to delete Learning Plan story");
-    }
-  }
-  async UpdateStory(updateId, workoutStoryData) {
-    try {
-      const accessToken = localStorage.getItem("accessToken");
-      const config = {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      };
-      const response = await axios.put(
-        `${BASE_URL}/workoutStatusUpdates/${updateId}`,
-        workoutStoryData,
-        config
-      );
-      return response.data;
-    } catch (error) {
-      throw new Error("Failed to update Learning Plan story");
-    }
-  }
+  
 }
 
 export default new StoryUpdateService();
